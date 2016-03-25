5fafd964d9a8f3d38721d9c6ff7483a8147ead1d
Nick Dewitte <nick.dewitte@student.howest.be>
2016-03-25 15:41:04 +0100

    test changes

--- src/net/ultradev/dominion/tests/CardTest.java
+++ src/net/ultradev/dominion/tests/CardTest.java
@@ -1,32 +1,68 @@
 package net.ultradev.dominion.tests;
 
-import net.ultradev.dominion.game.card.*;
+import java.util.List;
+
+import net.ultradev.dominion.game.card.Card;
+import net.ultradev.dominion.game.card.action.Action;
+import net.ultradev.dominion.game.card.action.actions.GainActionsAction;
+import net.ultradev.dominion.game.card.action.actions.GainBuypowerAction;
+import net.ultradev.dominion.game.card.action.actions.GainBuypowerAction.GainBuypowerType;
 
 public class CardTest {
 	
 	public Card c1;
+	public Card c2;
 	public boolean Testing = true;
 	
 	public void initTest(){
 		//public Card(String name, String description, int cost)
 		c1 = new Card("name","description",99);
+		c2 = new Card("name2","description2",98);
 		if(Testing){
-			System.out.println("Functie in de main toegevoegd?");
+			System.out.println("Functie in de main toegevoegd?\n");
 		}
 	}
-	/*
+	
 	public void testAddAction(){
-		//woodcutter.addAction(parseAction("add_buys", "Adds 1 buy to your turn", "amount=1"));
-		c1.addAction("test_game","Test the game","test=over9000");
+		Action action = new GainBuypowerAction("test_game","test something",9001, GainBuypowerType.ADD);
+		c1.addAction(action);
+		List<Action> actions = c1.getActions();
+		if(!(actions.contains(action))) {
+			System.out.println("Error: testAddAction");
+		};
 	}
-	*/
+	
+	public void testAddType(){
+		String type = "TestCard";
+		c1.addType(type);
+		List<String> types = c1.getTypes();
+		if(!(types.contains(type))) {
+			System.out.println("Error: testAddType");
+		}
+	}
+	
+	public void testGetTypesFormatted() {
+		String type = "anotherType";
+		String type2 = "yetAnotherType";
+		c2.addType(type);
+		c2.addType(type2);
+		String arrangedTypes = c2.getTypesFormatted();
+		if(!(arrangedTypes.equals("anotherType - yetAnotherType"))) {   //Strings nooit vergelijken met "==" maar met [STRING].equals([STRING])
+			System.out.println("Error: testGetTypesFormatted");
+		}
+	}
+	
 	
 	
 	
 	public static void main(String[] args){
 		System.out.println("Starting test");
-		//CardTest ct = new CardTest();
-		System.out.println("Test ended");
+		CardTest ct = new CardTest();
+		ct.initTest();
+		ct.testAddAction();
+		ct.testAddType();
+		ct.testGetTypesFormatted();
+		System.out.println("\nTest ended");
 	}
 
 }
--- src/net/ultradev/dominion/tests/GameTest.java
+++ src/net/ultradev/dominion/tests/GameTest.java
@@ -1,32 +1,44 @@
 package net.ultradev.dominion.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.ultradev.dominion.GameServer;
 import net.ultradev.dominion.game.Board;
+import net.ultradev.dominion.game.GameConfig;
 
 public class GameTest {
 	
 	private GameServer gs;
-	public int playerAmount1;
-	public Board b1;
-	public boolean Testing = true;
+	private int playerAmount1;
+	private Board b1;
+	private GameConfig gc;
+	private GameConfig gc2;
+	private GameConfig gc3;
+	private boolean Testing = true;
 	
 	public GameTest() {
 		gs = new GameServer();
-		System.out.println("Starting test");
+		System.out.println("Starting test\n");
+		
 		//Test alle functies die niet afhankelijk zijn van het aantal spelers
 		initTest();
 		testAmountOfActionCards();
+		testActionCardTypes();
+		testHandleRemove();
+		
 		//Test alle functies die afhankelijk zijn van het aantal spelers
 		for(int i = 2;  i <= 4; i++) {
 			playerAmount1 = i;
 			initLoop();
-			testAanmakenCoppers();
-			testAantalEstates();
-			testAantalDuchies();
-			testAantalProvinces();
-			testAantalCurses();
+			testMakeCoppers();
+			testAmountEstates();
+			testAmountDuchies();
+			testAmountProvinces();
+			testAmountCurses();
+			testHandleAdd();
 		}
-		System.out.println("Test ended");
+		System.out.println("\nTest ended");
 	}
 	
 	public GameServer getGameServer() {
@@ -35,6 +47,7 @@ public class GameTest {
 	
 	public void initTest() {
 		makeBoard(playerAmount1);
+		makeGameConfig();
 		if(Testing){
 			System.out.println("Functie in de main toegevoegd?");
 		}
@@ -50,40 +63,50 @@ public class GameTest {
 		b1.initSupplies(players);
 	}
 	
-	public void testAanmakenCoppers() {
+	public void makeGameConfig() {
+		gc = new GameConfig();
+		gc2 = new GameConfig();
+		gc3 = new GameConfig();
+	}
+	
+	
+	
+	
+	
+	public void testMakeCoppers() {
 		int coppers = b1.treasuresupply.get(getGameServer().getCardManager().get("copper"));
 		int desiredCoppers = 60 - 7 * playerAmount1;
 		if (coppers != desiredCoppers){
-			System.out.println("Error: aanmakenCoppers playerAmount="+playerAmount1);
+			System.out.println("Error: testMakeCoppers playerAmount="+playerAmount1);
 			System.out.println("coppers = " + coppers + ", and should be = " + desiredCoppers);
 		}
 	}
 	
-	public void testAantalProvinces() {
+	public void testAmountProvinces() {
 		int provinces = b1.victorysupply.get(getGameServer().getCardManager().get("province"));
 		if ( (provinces != 8 && playerAmount1 == 2) || (provinces != 12 && playerAmount1 > 2 && playerAmount1 <= 4) ) {
-			System.out.println("Error: testAantalProvinces playerAmount="+playerAmount1);
+			System.out.println("Error: testAmountProvinces playerAmount="+playerAmount1);
 		}
 	}
 	
-	public void testAantalDuchies() {
+	public void testAmountDuchies() {
 		int duchies = b1.victorysupply.get(getGameServer().getCardManager().get("duchy"));
 		if ( (duchies != 8 && playerAmount1 == 2) || (duchies != 12 && playerAmount1 > 2 && playerAmount1 <= 4) ) {
-			System.out.println("Error: testAantalDuchies playerAmount="+playerAmount1);
+			System.out.println("Error: testAmountDuchies playerAmount="+playerAmount1);
 		}
 	}
 	
-	public void testAantalEstates() {
+	public void testAmountEstates() {
 		int estates = b1.victorysupply.get(getGameServer().getCardManager().get("estate"));
 		if ( (estates != 8 && playerAmount1 == 2) || (estates != 12 && playerAmount1 > 2 && playerAmount1 <= 4) ) {
-			System.out.println("Error: testAantalEstates playerAmount="+playerAmount1);
+			System.out.println("Error: testAmountEstates playerAmount="+playerAmount1);
 		}
 	}
 	
-	public void testAantalCurses() {
+	public void testAmountCurses() {
 		int curses = b1.cursesupply.get(getGameServer().getCardManager().get("curse"));
 		if ( (curses + 10) / playerAmount1 != 10) {
-			System.out.println("Error: testAantalCurses playerAmount="+playerAmount1);
+			System.out.println("Error: testAmountCurses playerAmount="+playerAmount1);
 		}
 	}
 	
@@ -93,6 +116,59 @@ public class GameTest {
 		int chapelCount = b1.actionsupply.get(getGameServer().getCardManager().get("chapel"));
 		if (chapelCount != 10) {
 			System.out.println("Error: testAmountOfActionCards");
+		}
+	}
+	
+	public void testHandleAdd() {
+		String key = "ADDCARD";
+		String value = "Garden";
+		boolean succes = gc.handle(key,value);
+		if(!succes) {
+			System.out.println("Error: testHandleAdd");
+		}
+	}
+	
+	public void testActionCardTypes() {
+		String value = null;
+		String key = "ADDCARD";
+		List<String> actionCards;
+		List<String> desiredResult = new ArrayList<>();
+		boolean succes = true;
+		for(int i = 0; i < 10; i++) {
+			desiredResult.add(Integer.toString(i));
+		}
+		for(int i = 0; i <= 11 && succes; i++) {
+			value = Integer.toString(i);
+			succes = gc2.handle(key, value);
+		}
+		actionCards = gc2.getActionCards();
+		if(!succes && actionCards == desiredResult) {
+			System.out.println("Error testActionCardTypes & cycle = " + value);
+		}
+	}
+	
+	public void testHandleRemove() {
+		String key = "addcard";
+		List<String> actionCards;
+		List<String> desiredResult = new ArrayList<>();
+		boolean succes = true;
+		String val = null;
+		for(int i = 0; i < 9; i++) {
+			val = Integer.toString(i);
+			desiredResult.add(val);
+		}
+		desiredResult.add("11");
+		for(int i = 0; i < 10 && succes; i++) {
+			val = Integer.toString(i);
+			succes = gc3.handle(key, val);
+		}
+		succes = gc3.handle("removecard", val);
+		val = "11";
+		succes = gc3.handle(key, val);
+		actionCards = gc3.getActionCards();
+		//System.out.println(actionCards);
+		if(!(actionCards == desiredResult || succes)) {
+			System.out.println("Error: testHandleRemove");
 		}
 	}
 
