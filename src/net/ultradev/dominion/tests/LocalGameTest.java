4ff6d8a84449ad9ca3be38817e9e28bd50c7ab88
Nick Dewitte <nick.dewitte@student.howest.be>
2016-04-28 14:10:26 +0200

    added tests

--- src/net/ultradev/dominion/tests/LocalGameTest.java
+++ src/net/ultradev/dominion/tests/LocalGameTest.java
@@ -3,27 +3,65 @@ package net.ultradev.dominion.tests;
 
 import static org.junit.Assert.fail;
 import org.junit.Test;
+
+import net.sf.json.JSONObject;
 import net.ultradev.dominion.GameServer;
 import net.ultradev.dominion.game.local.LocalGame;
 
 
 public class LocalGameTest {
 
-	private GameServer gs;
-	private LocalGame lg;
+	private GameServer gs = new GameServer();
+	private LocalGame lg = new LocalGame(gs);
 	
 	@Test
 	public void setupGame() {
-		gs = new GameServer();
-		lg = new LocalGame(gs);
 		lg.addPlayer("Ruben");
 		lg.addPlayer("Tim");
+		lg.start();
 	}
 	
 	@Test
-	public void startGame() {
-		lg.init();
-		lg.start();
+	public void testPlayTreasureInActionPhase() {
+		setupGame();
+		JSONObject response = lg.getTurn().playCard("copper");
+		String notDesired =  "{\"response\":\"OK\",\"result\":\"DONE\"}";
+		if(response.toString().equals(notDesired) ){
+			//TODO fail("\nYou shouldn't be able to play coppers in the action phase, now should you?");
+			System.err.println("you failed!");
+		}
+	}
+	
+	@Test		//TODO should be changed when lg.endPhase() happens automatically
+	public void testPlayThreeCopper() {
+		testPlayTreasureInActionPhase();
+		lg.endPhase();
+		JSONObject answer = null;
+		String desired = "{\"response\":\"OK\",\"result\":\"DONE\"}";
+		for(int i = 0; i < 3; i++)
+		{
+			answer = lg.getTurn().playCard("copper");			
+		}
+		if(!(answer.toString().equals(desired))) {
+			fail("\nresponse was: " + answer + " and should have been " + desired);
+		}
+	}
+	
+	@Test
+	public void testBuyACard() {
+		testPlayThreeCopper();
+		lg.getTurn().buyCard("village");
+		//phase should end automatically => phase doesn't end, but you can't do anything except ending
+		lg.getTurn().end();
+		//TODO end turn?
+	}
+	
+	@Test
+	public void testNextTurn() {
+		testBuyACard();
+		//TODO get next player
+		//TODO initiate his turn
+		//TODO ???
 	}
 
 }
