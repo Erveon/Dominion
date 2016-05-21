0e19f5321bcd27d043dd675d7a652f438cda2775
lolNope <nick.dewitte@student.howest.be>
2016-05-21 17:54:07 +0200

    Addes tests to check possible game ending triggers

--- src/net/ultradev/dominion/tests/EngineTest.java
+++ src/net/ultradev/dominion/tests/EngineTest.java
@@ -8,26 +8,68 @@ import org.junit.Test;
 import net.sf.json.JSONObject;
 import net.ultradev.dominion.GameServer;
 import net.ultradev.dominion.game.Game;
+import net.ultradev.dominion.game.GameConfig;
+import net.ultradev.dominion.game.card.Card;
+import net.ultradev.dominion.game.card.Supply;
+import net.ultradev.dominion.game.Board;
+import net.ultradev.dominion.game.Board.SupplyType;
 import net.ultradev.dominion.game.local.LocalGame;
+import net.ultradev.dominion.game.player.Player;
 
 public class EngineTest {
 
 	private GameServer gameServer;
 	private Game game;
-	
+	private Board board;
+	private GameConfig gameConfig;
+
 	@Before
 	public void init() {
 		gameServer = new GameServer();
 		game = new LocalGame(gameServer);
+		board = new Board(game);
+		gameConfig = game.getConfig();
 		game.addPlayer("Bob");
 		game.addPlayer("Jos");
+		gameConfig.setCardset("test");
 		game.start();
+	}
+
+	@Test
+	public void testPlayTreasureInActionPhase() {
+		JSONObject response = game.getTurn().playCard("copper");
+		assertTrue("Played treasure in action phase", response.toString().contains("invalid"));
+	}
+	// onderstaande endcondition tests geven een nulpointer exception in klasse Board op lijn 66.
+	
+	@Test
+	public void testEndConditionNoProvinces() {
+		Card province = gameServer.getCardManager().get("province");
+		for(int players = 2; players <= 4; players++) {
+			int playerCount = game.getPlayers().size();
+			int amountOfCards = playerCount == 2 ? 8 : 12;
+			for(int i = 0; i < amountOfCards; i++){
+				board.getSupply(SupplyType.ACTION).removeOne(province);
+			}
+			assertTrue("Game doesn't end when there are " + amountOfCards + " provinces removed", board.hasEndCondition());
+			String newPlayer = "player " + players;
+			game.addPlayer(newPlayer);
+			board.addActionCard(gameServer.getCardManager().get("province"));
+		}
 	}
 	
 	@Test
-	public void testPlayTreasureInActionPhase () {
-		JSONObject response = game.getTurn().playCard("copper");
-		assertTrue("Played treasure in action phase", response.toString().contains("invalid"));
+	public void testEndConditonThreeEmptyPiles() {
+		for(int players = 2; players <= 4; players++) {
+			int playerCount = game.getPlayers().size();
+			int amountOfCards = playerCount == 2 ? 8 : 12;
+			for(int i = 0; i < amountOfCards; i++) {
+				board.removeFromSupply(gameServer.getCardManager().get("curse"), SupplyType.ACTION);
+				board.removeFromSupply(gameServer.getCardManager().get("estate"), SupplyType.ACTION);
+				board.removeFromSupply(gameServer.getCardManager().get("duchy"), SupplyType.ACTION);
+			}
+			assertTrue("Game doesn't end when there are " + amountOfCards + " removed from 3 piles", board.hasEndCondition());
+		}
 	}
 
 }
